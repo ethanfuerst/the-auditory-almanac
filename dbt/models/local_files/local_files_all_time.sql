3a1125d3b2cc4b8ebe4eccf6112aa7ce3aeb738b
Ethan Fuerst <ethanfuerst@gmail.com>
2023-02-22 22:01:00 -0500

    linting

--- dbt/models/local_files/local_files_all_time.sql
+++ dbt/models/local_files/local_files_all_time.sql
@@ -7,27 +7,27 @@
 with
 
 final as (
-    select
-        local_file_id
-        , id as stream_id
-        , stream_start
-        , stream_end
-        , ms_played
-        , reason_start
-        , reason_end
-        , shuffle
-        , skipped
-        , offline
-        , incognito_mode
-    from {{ ref('streams') }}
-    where 1 = 1
-        and media_type = 'local file'
+	select
+		local_file_id
+		, id as stream_id
+		, stream_start
+		, stream_end
+		, ms_played
+		, reason_start
+		, reason_end
+		, shuffle
+		, skipped
+		, offline
+		, incognito_mode
+	from {{ ref('streams') }}
+	where 1 = 1
+		and media_type = 'local file'
 )
 
 select
-    *
+	*
 from final
 where 1 = 1
 order by
-    stream_start
-    , stream_end desc
+	stream_start asc
+	, stream_end desc
