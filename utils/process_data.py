18217dce08009ca21ec2f50e0f401847d817c686
Ethan Fuerst <34084535+ethanfuerst@users.noreply.github.com>
2024-07-26 21:51:44 -0400

    removing comments and adding button

--- utils/process_data.py
+++ utils/process_data.py
@@ -3,16 +3,12 @@ from dbt.cli.main import dbtRunner, dbtRunnerResult
 
 
 def build_tables() -> None:
-    # https://docs.getdbt.com/reference/programmatic-invocations
     dbt = dbtRunner()
     os.environ["DBT_PROJECT_DIR"] = "dbt_project/"
-    # same as "--project-dir dbt_project/"
 
-    # check for env vars https://docs.getdbt.com/reference/parsing#known-limitations
     cli_args = ["build"]
 
     os.makedirs(os.path.dirname("data/dbt.db"), exist_ok=True)
     res: dbtRunnerResult = dbt.invoke(cli_args)
-    # log res.result
 
     return
